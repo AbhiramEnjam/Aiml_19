8140cf0dcdd136f3fc12f4bfeedb9c94abeef1ec
online lab <online lab@DESKTOP-6UAU2HI>
2023-06-08 22:19:10 -0700

    1st commit "hi"

---
+++ Account_data/src/Account_data/Friend_ship.java
@@ -0,0 +1,43 @@
+package Account_data;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Friend_ship {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Friend_ship window = new Friend_ship();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Friend_ship() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
